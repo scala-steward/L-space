63914fbaa1166fab58b9f1cbb46da30a558f1be5
Thijs Broersen <mht.broersen@gmail.com>
2019-01-03 01:42:58 +0100

    removing old files

--- core/shared/src/main/scala/lspace/librarian/structure/util/PropertyDef.scala
+++
@@ -1,9 +0,0 @@
-package lspace.librarian.structure.util
-
-import lspace.librarian.structure.Property
-
-//object PropertyDef {
-//  implicit def defToProperty(propDef: PropertyDef): Property = propDef.value
-//}
-//
-//trait PropertyDef { def value: Property }
