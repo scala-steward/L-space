12f947a699d3298f818ad70ceca3964cf51ded13
Thijs Broersen <mht.broersen@gmail.com>
2019-05-02 00:51:20 +0200

    renamed step to steps

--- core/shared/src/main/scala/lspace/librarian/traversal/Segment.scala
+++ core/shared/src/main/scala/lspace/librarian/traversal/Segment.scala
@@ -14,22 +14,22 @@ object Segment
 
   object keys {
 
-    object step
+    object steps
         extends PropertyDef(
-          lspace.NS.vocab.Lspace + "librarian/TraversalSegment/step",
-          "step",
-          "A step in a traversal",
+          lspace.NS.vocab.Lspace + "librarian/TraversalSegment/steps",
+          "steps",
+          "The steps in a traversal-segment",
           `@range` = () => VectorType(Step.ontology :: Nil) :: Nil
         ) {}
 
-    lazy val stepNode: TypedProperty[Vector[Node]] = step.property + VectorType(Step.ontology :: Nil)
+    lazy val stepsNode: TypedProperty[Vector[Node]] = steps.property + VectorType(Step.ontology :: Nil)
   }
 
-  override lazy val properties: List[Property] = keys.step :: Nil
+  override lazy val properties: List[Property] = keys.steps :: Nil
 
   trait Properties {
-    lazy val `ns.l-space.eu/librarian/TraversalSegment/step`: Property                    = keys.step
-    lazy val `ns.l-space.eu/librarian/TraversalSegment/step@Node`: TypedKey[Vector[Node]] = keys.stepNode
+    lazy val `ns.l-space.eu/librarian/TraversalSegment/steps`: Property                    = keys.steps
+    lazy val `ns.l-space.eu/librarian/TraversalSegment/steps@Node`: TypedKey[Vector[Node]] = keys.stepsNode
   }
 
   def apply(): Segment[HNil] = new Segment(HNil)
@@ -41,7 +41,7 @@ object Segment
 
     for {
       steps0 <- Task
-        .gather(node.out(Segment.keys.stepNode).take(1).flatMap(_.toList).map(Step.toStep))
+        .gather(node.out(Segment.keys.stepsNode).take(1).flatMap(_.toList).map(Step.toStep))
         .map(_.foldLeft[HList](HNil) {
           case (hlist, step) => step :: hlist
         })
@@ -62,7 +62,7 @@ case class Segment[Steps <: HList] protected[lspace] (steps: Steps) {
     for {
       node  <- DetachedGraph.nodes.create(Segment.ontology)
       steps <- Task.gather(stepsList.map(_.toNode).toVector)
-      e     <- node.addOut(Segment.keys.stepNode, steps)
+      e     <- node.addOut(Segment.keys.stepsNode, steps)
     } yield node
   }
 
