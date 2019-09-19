7fee0237a8a36f682164708478167e9e673691fe
Scala Steward <me@scala-steward.org>
2019-09-19 02:27:37 +0200

    Update fs2-reactive-streams to 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -79,7 +79,7 @@ object Dependencies {
     "com.github.finagle" %% "finchx-argonaut"      % Version.finch,
     "com.github.finagle" %% "finchx-fs2"           % Version.finch,
     "com.github.finagle" %% "finchx-refined"       % Version.finch,
-    "co.fs2"             %% "fs2-reactive-streams" % "1.0.5",
+    "co.fs2"             %% "fs2-reactive-streams" % "2.0.0",
 //    "com.twitter"        %% "twitter-server"       % "19.4.0" % "test",
     "com.vmunier" %% "scalajs-scripts" % "1.1.4",
     //    "com.github.t3hnar" %% "scala-bcrypt" % "3.1",
