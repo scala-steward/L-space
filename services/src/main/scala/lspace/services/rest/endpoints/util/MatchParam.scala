7f71b4803f52863e08034907f15a8601caaac585
Thijs Broersen <mht.broersen@gmail.com>
2019-09-23 23:45:11 +0200

    filter for query parameters

---
+++ services/src/main/scala/lspace/services/rest/endpoints/util/MatchParam.scala
@@ -0,0 +1,25 @@
+package lspace.services.rest.endpoints.util
+
+import cats.Applicative
+import io.finch.{Endpoint, EndpointResult, Input, Output, Trace}
+import shapeless.HNil
+
+class MatchParam[F[_]](name: String)(implicit
+                                     F: Applicative[F])
+    extends Endpoint[F, HNil] {
+  final def apply(input: Input): EndpointResult[F, HNil] = {
+    if (input.request.containsParam(name))
+      EndpointResult.Matched(
+        input,
+        Trace.empty,
+        F.pure(Output.HNil)
+      )
+    else EndpointResult.NotMatched[F]
+  }
+}
+
+object MatchParam {
+  def apply[F[_]](name: String)(implicit
+                                F: Applicative[F]): MatchParam[F] =
+    new MatchParam[F](name)
+}
