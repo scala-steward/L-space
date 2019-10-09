85e36347ce7812f43c6cd9ad987cd97b3b617cc8
Thijs Broersen <mht.broersen@gmail.com>
2019-10-09 22:25:51 +0200

    .

--- core/shared/src/test/scala/lspace/structure/DataTypeSpec.scala
+++ core/shared/src/test/scala/lspace/structure/DataTypeSpec.scala
@@ -1,5 +1,14 @@
 package lspace.structure
 
+import lspace.datatype.DataType
+import lspace.Label.D._
 import org.scalatest.{AsyncWordSpec, Matchers}
 
-class DataTypeSpec extends AsyncWordSpec with Matchers {}
+class DataTypeSpec extends AsyncWordSpec with Matchers {
+
+  "DataType" should {
+    "dt" in {
+      DataType.datatypes.get("http://schema.org/Text") shouldBe Some(`@string`)
+    }
+  }
+}
