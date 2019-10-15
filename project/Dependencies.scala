1f3e669723148457928e8ca35ec67cb12cc21bc0
Scala Steward <me@scala-steward.org>
2019-10-15 13:46:23 +0200

    Update scalatest to 3.1.0-RC3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
 //      "org.typelevel" %%% "squants"        % "1.5.0",
 //      "org.typelevel" %%% "spire"          % "0.16.0",
       "com.outr"          %%% "scribe"          % "2.7.10",
-      "org.scalatest"     %%% "scalatest"       % "3.1.0-RC2" % "test",
+      "org.scalatest"     %%% "scalatest"       % "3.1.0-RC3" % "test",
       "io.monix"          %%% "minitest"        % "2.7.0" % "test",
       "io.github.cquiroz" %%% "scala-java-time" % "2.0.0-RC3"
     ))
