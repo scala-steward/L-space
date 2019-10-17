b56bf6a48b2715413c49e57bf8e066d7f8a56693
Thijs Broersen <mht.broersen@gmail.com>
2019-10-17 13:33:51 +0200

    .

--- parse/core/shared/src/test/scala/lspace/codec/json/geojson/EncoderSpec.scala
+++ parse/core/shared/src/test/scala/lspace/codec/json/geojson/EncoderSpec.scala
@@ -20,20 +20,32 @@ abstract class EncoderSpec[Json](val encoder: JsonEncoder[Json]) extends AsyncWo
   }
   def geojsonencoder: GeoJsonEncoder[Json]
 
-  "An GeoJsonEncoder" should {
+  "An GeoJsonEncoder" can {
     "encode a point" in Future {
       geojsonencoder
         .encodePointObject(Point(102.0, 0.5)) shouldBe """{"type":"Point","coordinates":[102,0.5]}""".parse
+      geojsonencoder
+        .encodePoint(Point(102.0, 0.5)) shouldBe """[102,0.5]""".parse
     }
     "encode a multipoint" in Future {
+      geojsonencoder
+        .encodeMultiPoint(MultiPoint(Point(10, 40), Point(40, 30), Point(20, 20), Point(30, 10))) shouldBe """[[10,40],[40,30],[20,20],[30,10]]""".parse
       geojsonencoder
         .encodeMultiPointObject(MultiPoint(Point(10, 40), Point(40, 30), Point(20, 20), Point(30, 10))) shouldBe """{"type":"MultiPoint","coordinates":[[10,40],[40,30],[20,20],[30,10]]}""".parse
     }
     "encode a line" in Future {
       geojsonencoder
+        .encodeLine(Line(Point(102, 0), Point(103, 1), Point(104, 0), Point(105, 1))) shouldBe """[[102,0],[103,1],[104,0],[105,1]]""".parse
+      geojsonencoder
         .encodeLineObject(Line(Point(102, 0), Point(103, 1), Point(104, 0), Point(105, 1))) shouldBe """{"type":"LineString","coordinates":[[102,0],[103,1],[104,0],[105,1]]}""".parse
     }
     "encode a multiline" in Future {
+      geojsonencoder
+        .encodeMultiLine(
+          MultiLine(
+            Line(Point(10, 10), Point(20, 20), Point(10, 40)),
+            Line(Point(40, 40), Point(30, 30), Point(40, 20), Point(30, 10)))) shouldBe """[[[10,10],[20,20],[10,40]],[[40,40],[30,30],[40,20],[30,10]]]""".parse
+
       geojsonencoder
         .encodeMultiLineObject(
           MultiLine(
@@ -42,9 +54,19 @@ abstract class EncoderSpec[Json](val encoder: JsonEncoder[Json]) extends AsyncWo
     }
     "encode a polygon" in Future {
       geojsonencoder
+        .encodePolygon(Polygon(Point(100, 0), Point(101, 0), Point(101, 1), Point(100, 1), Point(100, 0))) shouldBe """[[[100,0],[101,0],[101,1],[100,1],[100,0]]]""".parse
+      geojsonencoder
         .encodePolygonObject(Polygon(Point(100, 0), Point(101, 0), Point(101, 1), Point(100, 1), Point(100, 0))) shouldBe """{"type":"Polygon","coordinates":[[[100,0],[101,0],[101,1],[100,1],[100,0]]]}""".parse
     }
     "encode a multipolygon" in Future {
+      geojsonencoder
+        .encodeMultiPolygon(MultiPolygon(Polygon(Point(30, 20), Point(45, 40), Point(10, 40), Point(30, 20)),
+                                         Polygon(
+                                           Point(15, 5),
+                                           Point(40, 10),
+                                           Point(10, 20),
+                                           Point(5, 10),
+                                           Point(15, 5)))) shouldBe """[[[[30,20],[45,40],[10,40],[30,20]]],[[[15,5],[40,10],[10,20],[5,10],[15,5]]]]""".parse
       geojsonencoder
         .encodeMultiPolygonObject(MultiPolygon(Polygon(Point(30, 20), Point(45, 40), Point(10, 40), Point(30, 20)),
                                                Polygon(
@@ -55,6 +77,12 @@ abstract class EncoderSpec[Json](val encoder: JsonEncoder[Json]) extends AsyncWo
                                                  Point(15, 5)))) shouldBe """{"type":"MultiPolygon","coordinates":[[[[30,20],[45,40],[10,40],[30,20]]],[[[15,5],[40,10],[10,20],[5,10],[15,5]]]]}""".parse
     }
     "encode a multigeometry" in Future {
+      geojsonencoder
+        .encodeMultiGeometry(
+          MultiGeometry(
+            Point(40, 10),
+            Line(Point(10, 10), Point(20, 20), Point(10, 40)),
+            Polygon(Point(40, 40), Point(20, 45), Point(45, 30), Point(40, 40)))) shouldBe """[{"type":"Point","coordinates":[40,10]},{"type":"LineString","coordinates":[[10,10],[20,20],[10,40]]},{"type":"Polygon","coordinates":[[[40,40],[20,45],[45,30],[40,40]]]}]""".parse
       geojsonencoder
         .encodeMultiGeometryObject(
           MultiGeometry(
