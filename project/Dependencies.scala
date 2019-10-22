c78fd199d43769479d3afe113f14e4e58414d2c4
Scala Steward <me@scala-steward.org>
2019-10-22 22:44:26 +0200

    Update circe-parser to 0.12.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
 
   val parseArgonautDeps = Def.setting(Seq("io.argonaut" %%% "argonaut" % "6.2.3"))
 
-  val parseCirceDeps = Def.setting(Seq("io.circe" %%% "circe-parser" % "0.12.2"))
+  val parseCirceDeps = Def.setting(Seq("io.circe" %%% "circe-parser" % "0.12.3"))
 
   val clientDeps = Def.setting(Seq())
 
