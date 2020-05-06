9bf8351c41f507f2b5d5940d9749cf21d423989c
Thijs Broersen <mht.broersen@gmail.com>
2020-05-06 19:57:01 +0200

    update

--- parse/argonaut/js/src/test/scala/lspace/parse/test/package.scala
+++ parse/argonaut/js/src/test/scala/lspace/parse/test/package.scala
@@ -2,8 +2,9 @@ package lspace.parse
 
 package object test {
   def init(): Unit = {
-    scalajs.js.Dynamic.global.globalThis.fetch = scalajs.js.Dynamic.global.require("node-fetch")
-    scalajs.js.Dynamic.global.require("abortcontroller-polyfill/dist/polyfill-patch-fetch")
-    scalajs.js.Dynamic.global.globalThis.Headers = scalajs.js.Dynamic.global.require("fetch-headers")
+    val g = scalajs.js.Dynamic.global.globalThis
+    g.fetch = g.require("node-fetch")
+    g.require("abortcontroller-polyfill/dist/polyfill-patch-fetch")
+    g.Headers = g.require("fetch-headers")
   }
 }
